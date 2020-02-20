c0e1cd38ce8207b81a733d3d55d482e0a83c08e3
gkahl <gkahl@gwu.edu>
2020-02-20 12:34:43 -0500

    Fixed another bug I made in demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import blobTrace
 import blobPath
 
-blobPath.path("test/bounce.mp4")
+blobPath.path("test_vids/bounce.mp4")
 blobTrace.trace("test_vids/1throw.mp4")
 blobTrace.trace("test_vids/2ball.mp4")
 blobTrace.trace("test_vids/3ball.mp4")
