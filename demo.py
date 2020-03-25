50c4d9a9fc07c2b01d887595f6683464f2ce1ce4
gkahl <gkahl@gwu.edu>
2020-03-24 21:38:41 -0400

    Two new juggling videos

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import blobTrace
 import blobPath
 
-blobTrace.trace("test_vids/quarantine_1.mp4",1)
+blobTrace.trace("test_vids/quarantine_2.mp4",1)
 '''
 blobTrace.trace("test_vids/2ball.mp4", 1)
 blobTrace.trace("test_vids/3ball.mp4", 1)
